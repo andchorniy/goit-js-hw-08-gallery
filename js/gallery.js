5e3b1a3561d633de3b0486ffa08321324fcedb09
Andrii Chornyi <andchorniy@gmail.com>
2021-01-30 00:29:55 +0100

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -6,16 +6,18 @@ const originalImageView = document.querySelector('.lightbox__image');
 const closingModalBtnRef = document.querySelector('button[data-action="close-lightbox"]');
 const overlayRef = document.querySelector('.lightbox__overlay');
 
-const string = images.reduce((acc, image) => acc +=
-    `<li class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" data-source="${image.original}" src="${image.preview}"
+const string = images.reduce((acc, image, index) => acc +=
+    `<li class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" data-source="${image.original}" data-index="${index}" src="${image.preview}"
         alt="${image.description}" ></a></li>`
     , '')
      
 galleryRef.insertAdjacentHTML("afterbegin", string)
 
 function closingModalPerEsc (event) {
-    if (event.key === "Escape") closingModal()
-    window.removeEventListener('keydown', closingModalPerEsc)
+    if (event.key === "Escape") {
+        closingModal()
+        window.removeEventListener('keydown', closingModalPerEsc)
+    }
 }
 
 function closingModal()  {
@@ -23,14 +25,19 @@ function closingModal()  {
     originalImageView.src = ''
     originalImageView.alt = ''
 }
-
+function nextImage(event) { 
+    if (!event.key === "ArrowRight") return
+    
+}
 galleryRef.addEventListener('click', (event) => { 
     event.preventDefault()
     if (!event.target.classList.contains('gallery__image')) return
     modalRef.classList.add('is-open')
     originalImageView.src = event.target.dataset.source
     originalImageView.alt = event.target.alt
-    window.addEventListener('keydown',closingModalPerEsc )
+    originalImageView.dataset.index = event.target.dataset.index
+    window.addEventListener('keydown', closingModalPerEsc)
+    window.addEventListener('keydown', nextImage)
 })
 
 closingModalBtnRef.addEventListener('click', closingModal)
