ad5db6348bfa0d7789d17c36816076fa08e6811d
Andrii Chornyi <andchorniy@gmail.com>
2021-01-30 01:14:16 +0100

    infinity gallery

--- js/gallery.js
+++ js/gallery.js
@@ -6,12 +6,18 @@ const originalImageView = document.querySelector('.lightbox__image');
 const closingModalBtnRef = document.querySelector('button[data-action="close-lightbox"]');
 const overlayRef = document.querySelector('.lightbox__overlay');
 
+
+
+
+
 const string = images.reduce((acc, image, index) => acc +=
     `<li class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" data-source="${image.original}" data-index="${index}" src="${image.preview}"
         alt="${image.description}" ></a></li>`
     , '')
      
 galleryRef.insertAdjacentHTML("afterbegin", string)
+
+const imagesRefs = document.querySelectorAll('.gallery__image');
 
 function closingModalPerEsc (event) {
     if (event.key === "Escape") {
@@ -24,11 +30,40 @@ function closingModal()  {
     modalRef.classList.remove('is-open')
     originalImageView.src = ''
     originalImageView.alt = ''
+    window.removeEventListener('keydown', nextImage)
+    window.removeEventListener('keydown', prevImage)
+
 }
 function nextImage(event) { 
-    if (!event.key === "ArrowRight") return
-    
+    if (event.key === "ArrowRight") {
+        const arrImages = Array.from(imagesRefs)
+        let nextImg 
+        if (+originalImageView.dataset.index === arrImages.length - 1) {
+            nextImg = arrImages[0]
+        }
+        else {
+             nextImg = arrImages.find(img => +img.dataset.index === (+originalImageView.dataset.index + 1))      
+        }
+        originalImageView.src = nextImg.dataset.source
+        originalImageView.alt = nextImg.alt
+        originalImageView.dataset.index = nextImg.dataset.index}
 }
+
+function prevImage(event) { 
+    if (event.key === "ArrowLeft") {
+        const arrImages = Array.from(imagesRefs)
+        let nextImg
+        if (+originalImageView.dataset.index === 0) {
+            nextImg = arrImages[arrImages.length - 1]
+        } else { 
+            nextImg = arrImages.find(img => +img.dataset.index === (+originalImageView.dataset.index - 1))
+        }
+        originalImageView.src = nextImg.dataset.source
+        originalImageView.alt = nextImg.alt
+        originalImageView.dataset.index = nextImg.dataset.index
+    }
+}
+
 galleryRef.addEventListener('click', (event) => { 
     event.preventDefault()
     if (!event.target.classList.contains('gallery__image')) return
@@ -38,6 +73,7 @@ galleryRef.addEventListener('click', (event) => {
     originalImageView.dataset.index = event.target.dataset.index
     window.addEventListener('keydown', closingModalPerEsc)
     window.addEventListener('keydown', nextImage)
+    window.addEventListener('keydown', prevImage)
 })
 
 closingModalBtnRef.addEventListener('click', closingModal)
